d740d6c17a94e6e6d1310815dad4f5089758e59c
Brittany Walker <breneewalker@gmail.com>
2021-02-27 14:20:55 -0500

    Update styling for hero and statistics section and linter fixes

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,282 +1,294 @@
 <template>
   <div class="hero">
-    <h1>TheaterLog</h1>
-    <div class="statistics">
-      <div class="statistics-content">
-        <div class="stats-intro">
-          <h2>Overview</h2>
-          <p>Since watching Grease for the first time in 5th grade, I have been hooked on musicals! This app was built to keep track of all of the musicals and plays I have seen since 2010.</p>
-          <p> Click on the show names to learn more about the show or current productions. And click on the song names to see a video of my favorite songs from each show!</p>
-          <p>The <font-awesome-icon icon="star" class="fav-icon" /> represents favorite shows and the <span class="multi-example">4</span> is the number of times I have seen a show.</p>
-          <p class="note">* Upcoming shows are gray!</p>
+    <header>
+      <div class="overview">
+        <h1>Theater Log</h1>
+        <div class="overview-text">
+          <p>
+            Since watching Grease for the first time in 5th grade, I have been
+            hooked on musicals! This app was built to keep track of all of the
+            musicals and plays I have seen since 2010.
+          </p>
+          <p>
+            Click on the show names to learn more about the show or current
+            productions. And click on the song names to see a video of my
+            favorite songs from each show!
+          </p>
+          <p class="portfolio-link">
+            Created by
+            <a href="https://musicalwebdev.com" target="_blank"
+              >Brittany Walker.</a
+            >
+          </p>
         </div>
-        <div class="stats">
-          <h3>Statistics</h3>
-          <div class="counter total-stat"><p>Total Shows Seen</p> <p>{{ shows.length + 26}}</p></div>
-          <div class="counter upcoming-stat"><p>Upcoming </p><p>{{ upcomingCounter }}</p></div>
-          <div class="counter musical-stat"><p>Musicals</p> <p>{{ musicalsCounter + 26}}</p></div>
-          <div class="counter play-stat"><p>Plays</p> <p>{{ playsCounter}}</p></div> 
-          <div class="counter price-stat"><p>Total Spent</p> <p>${{ priceCounter.toLocaleString() }}</p></div> 
-          <div class="counter price-stat"><p>Average Ticket Cost</p> <p>${{ Math.floor(priceCounter/shows.length) }}</p></div> 
-        </div>
-        <p class="portfolio-link">Created by <a href="https://musicalwebdev.com" target="_blank">Brittany Walker.</a></p>    
       </div>
-      <div class="window">
-        <div class="review-legend">
-          <h3>Review Legend</h3>
+      <div class="hero-sidebar">
+        <div class="external-links">
+          <h4>Other Logs</h4>
+          <a href="https://thebookishlog.com" target="_blank">Bookish Log</a>
+        </div>
+        <div class="info">
           <ul>
             <li>
-              <img alt="Vue logo" src="./../assets/love.svg">
+              <font-awesome-icon icon="star" class="fav-icon" /> = favorite
+              shows
+            </li>
+            <li>
+              <span class="multi-example">4</span> = times a show was seen
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+    <div class="statistics">
+      <div class="statistics-content">
+        <h3>Statistics</h3>
+        <div class="stats">
+          <div class="counter total-stat">
+            <p class="stat-number">{{ shows.length + 26 }}</p>
+            <p class="stat-title">Total Shows Seen</p>
+          </div>
+          <div class="counter upcoming-stat">
+            <p class="stat-number">{{ upcomingCounter }}</p>
+            <p class="stat-title">Upcoming</p>
+          </div>
+          <div class="counter musical-stat">
+            <p class="stat-number">{{ musicalsCounter + 26 }}</p>
+            <p class="stat-title">Musicals</p>
+          </div>
+          <div class="counter play-stat">
+            <p class="stat-number">{{ playsCounter }}</p>
+            <p class="stat-title">Plays</p>
+          </div>
+          <div class="counter price-stat">
+            <p class="stat-number">${{ priceCounter.toLocaleString() }}</p>
+            <p class="stat-title">Total Spent</p>
+          </div>
+          <div class="counter price-stat">
+            <p class="stat-number">
+              ${{ Math.floor(priceCounter / shows.length) }}
+            </p>
+            <p class="stat-title">Average Ticket Cost</p>
+          </div>
+        </div>
+      </div>
+      <div class="stats-sidebar">
+        <div class="review-legend">
+          <h3>Review Legend</h3>
+          <ul class="review-emoji-list">
+            <li>
+              <img alt="love emoji" src="./../assets/love.svg" />
               <p>love</p>
             </li>
             <li>
-              <img alt="Vue logo" src="./../assets/happy.svg">
+              <img alt="happy emoji" src="./../assets/happy.svg" />
               <p>like</p>
             </li>
             <li>
-              <img alt="Vue logo" src="./../assets/funny.svg">
+              <img alt="funny emoji" src="./../assets/funny.svg" />
               <p>funny</p>
             </li>
             <li>
-              <img alt="Vue logo" src="./../assets/happy-sad.svg">
-              <p>sad but happy</p>
-            </li>
-            <li>
-              <img alt="Vue logo" src="./../assets/thought-provoking.svg">
-              <p>thought provoking</p>
-            </li>
-            <li>
-              <img alt="Vue logo" src="./../assets/sad.svg">
+              <img alt="sad emoji" src="./../assets/sad.svg" />
               <p>sad</p>
-            </li> 
+            </li>
             <li>
-              <img alt="Vue logo" src="./../assets/confused.svg">
+              <img alt="confused emoji" src="./../assets/confused.svg" />
               <p>confused</p>
             </li>
             <li>
-              <img alt="Vue logo" src="./../assets/meh.svg">
+              <img alt="meh emoji" src="./../assets/meh.svg" />
               <p>meh</p>
-            </li> 
+            </li>
             <li>
-              <img alt="Vue logo" src="./../assets/dislike.svg">
+              <img alt="dislike emoji" src="./../assets/dislike.svg" />
               <p>dislike</p>
-            </li>                               
-          </ul>                  
-        </div> 
-      </div>	
-    </div>		
+            </li>
+            <li>
+              <img
+                alt="happy but also sad emoji"
+                src="./../assets/happy-sad.svg"
+              />
+              <p>sad but happy</p>
+            </li>
+            <li>
+              <img
+                alt="thought provoking emoji"
+                src="./../assets/thought-provoking.svg"
+              />
+              <p>thought provoking</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
-import {showList} from "../data/shows";
+import { showList } from "../data/shows";
 
 export default {
-  name: 'Hero',
-    data: function () {
+  name: "Hero",
+  data: function() {
     return {
-      shows: showList
-    }
+      shows: showList,
+    };
   },
-  computed:{
-    musicalsCounter:function(){
-      var result = showList.reduce((res, item) => item.type == 'musical' ? res + 1 : res, 0);
-      return result;
-    }, 
-    playsCounter:function(){
-      var result = showList.reduce((res, item) => item.type == 'play' ? res + 1 : res, 0);
-      return result;
-    },  
-    upcomingCounter:function(){
-      var result = showList.reduce((res, item) => item.upcoming ? res + item.upcoming : res, 0);
+  computed: {
+    musicalsCounter: function() {
+      var result = showList.reduce(
+        (res, item) => (item.type == "musical" ? res + 1 : res),
+        0
+      );
       return result;
     },
-      priceCounter:function(){
-      var result = showList.reduce((res, item) => item.price ? res + item.price : res, 0);
+    playsCounter: function() {
+      var result = showList.reduce(
+        (res, item) => (item.type == "play" ? res + 1 : res),
+        0
+      );
+      return result;
+    },
+    upcomingCounter: function() {
+      var result = showList.reduce(
+        (res, item) => (item.upcoming ? res + item.upcoming : res),
+        0
+      );
+      return result;
+    },
+    priceCounter: function() {
+      var result = showList.reduce(
+        (res, item) => (item.price ? res + item.price : res),
+        0
+      );
       return Math.floor(result);
-    }
+    },
   },
-}
+};
 </script>
 
-<style>
-  .statistics {
-    background-color: white;
-    padding: 60px 30px 30px 30px;
-    border: 10px solid #6661af;
-    display: grid;
-    grid-template-columns: 1.5fr .5fr;
-    grid-auto-rows: auto;
-    text-align: left;
+<style lang="scss">
+@import "@/assets/styles/variables.scss";
+@import url("https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap");
+
+.hero {
+  header {
+    border-bottom: 3px solid $black;
+    @media screen and (min-width: 662px) {
+      display: flex;
+    }
+    .overview {
+      padding: 20px;
+      display: flex;
+      align-items: center;
+      flex-wrap: wrap;
+      @media screen and (min-width: 662px) {
+        flex-basis: 75%;
+        border-right: 3px solid $black;
+      }
+      h1 {
+        font-family: "Abril Fatface", cursive;
+        margin: 0;
+        letter-spacing: 1.5px;
+        font-size: 40px;
+      }
+    }
+    .hero-sidebar {
+      @media screen and (min-width: 662px) {
+        width: 300px;
+      }
+
+      .external-links,
+      .info {
+        padding: 20px;
+      }
+
+      .external-links {
+        border-top: 3px solid $black;
+        border-bottom: 3px solid $black;
+        @media screen and (min-width: 662px) {
+          border-top: 0;
+        }
+      }
+
+      .info {
+        .fa-star {
+          font-size: 24px;
+        }
+        .multi-example {
+          border: 2px solid $black;
+          padding: 5px;
+          display: inline-block;
+        }
+        li:first-child {
+          margin-bottom: 10px;
+        }
+      }
+    }
+  }
+}
+
+.statistics {
+  border-bottom: 3px solid $black;
+  .statistics-content {
+    background-color: $black;
+    color: $purple;
+    padding: 20px;
+    h3 {
+      margin: 0;
+    }
+    .stats {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-wrap: wrap;
+      padding: 20px 0;
+      @media screen and (min-width: 662px) {
+        padding: 20px;
+      }
+      .counter {
+        background-color: $purple;
+        text-align: center;
+        margin: 10px;
+        border: 2px solid $purple;
+        -webkit-box-shadow: 5px 5px 0 $purple;
+        box-shadow: 9px 9px 0 $purple;
+        border-radius: 7px;
+        color: $black;
+        outline: 3px solid $black;
+        position: relative;
+        .stat-number {
+          font-size: 50px;
+          font-weight: bold;
+          margin: 0;
+          padding: 10px 15px 0 15px;
+        }
+        .stat-title {
+          margin: 0;
+          padding: 0 15px 30px 15px;
+        }
+      }
+    }
   }
 
-  .portfolio-link a {
-    color: #6661af;
-  }
-
-  .stats-intro .multi-example {
-    background-color: #908ae2;
-		padding: 5px 10px;
-		font-weight: bold;
-    font-size: 21px;
-		border: 2px solid #402e47;
-    display: inline-block;
-  }
-
-  .stats-intro .multi-example:hover, .stats-intro .fav-icon:hover {
-    transform: rotate(5deg);
-  }
-
-
-  h1 {
-    font-family: 'Amaranth', sans-serif;
-    padding: 12px 100px;
-    display: inline-block;
-    font-size: 60px;
-    background-color: white;
-    margin-bottom: -50px;
-    color: #6661af;
-    border: 8px solid #6661af;
-    position: relative;
-  } 
-
-  h1:after {
-    position: absolute;
-    content: '';
-    height: 214%;
-    width: 131%;
-    top: -128%;
-    left: -15%;
-    z-index: -1;
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-image: url('~@/assets/marquee.svg');
-  }
-
-  h2 {
-    margin-top: 0;
-  }
-
-  p {
-    margin-bottom: 20px;
-  }
-
-  .fav-icon {
-    color: gold;
-    font-size: 30px;
-  }
-
-  .fav-icon path {
-    stroke: #402e47;
-    stroke-width: 25;
-  }
-
-  .stats-intro {
-    padding-right: 20px;
-    margin-bottom: 30px;
-  }
-
-  .note {
-    font-size: 16px;
-  }
-
-  .stats .counter {
-    display: inline-block;
-    border: 2px solid #402e47;
-    margin-right: 10px;
-    margin-bottom: 10px;
-  }
-
-  .stats .counter p {
-    display: inline-block;
-    margin: 0;
-    padding: 10px;
-    font-size: 18px;
-  }
-
-  .stats .counter p:last-child {
-    border-left: 2px solid #402e47;
-  }
-
-  .stats .total-stat p:last-child {
-    background-color: lightpink;
-  }
-
-  .stats .upcoming-stat p:last-child {
-    background-color: gold;
-  }
-
-  .stats .musical-stat p:last-child, .stats .price-stat p:last-child {
-    background-color: #7ed6c9;
-  }
-
-  .stats .play-stat p:last-child {
-    background-color: #908ae2;
-  }
-
-  .review-legend ul {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-  }
-
-  .review-legend li {
-    padding: 5px;
-    margin-right: 5px;
-    display: inline-block;
+  .review-legend {
+    padding: 20px;
+    border-top: 3px solid $black;
     text-align: center;
-  }
-
-  .review-legend li:first-child p, .review-legend li:nth-child(7) p {
-    background-color: lightpink;
-  }
-
-  .review-legend li:nth-child(2) p, .review-legend li:nth-child(8) p, .review-legend li:nth-child(5) p {
-    background-color: gold;
-  }
-
-  .review-legend li:nth-child(3) p, .review-legend li:nth-child(6) p {
-    background-color: #908ae2;
-  }
-
-  .review-legend li:nth-child(4) p, .review-legend li:nth-child(9) p {
-    background-color: #7ed6c9;
-  }
-
-  .review-legend li:nth-child(even):hover {
-    transform: rotate(5deg);
-  }
-
-  .review-legend li:nth-child(odd):hover {
-    transform: rotate(-5deg);
-  }
-
-  .review-legend p {
-    font-size: 11px;
-    padding: 5px;
-    border: 2px solid #402e47;
-    display: inline-block;
-  }
-  .review-legend img {
-      height: 30px;
-      display: block;
-      margin: auto;
-  }
-
-  @media screen and (max-width: 992px){ 
-    .statistics, .review-ledgend ul {
-      grid-template-columns: 1fr;
+    .review-emoji-list {
+      li {
+        text-align: center;
+        margin: 10px;
+        p {
+          border: 2px solid black;
+          padding: 5px;
+        }
+        img {
+          width: 40px;
+        }
+      }
     }
-    h1 {
-      font-size: 50px;
-      padding: 12px 20px;
-    }
-    h1:after {
-      height: 189%;
-      width: 146%;
-      top: -75%;
-      left: -23%;
-    }
-
   }
+}
 </style>
