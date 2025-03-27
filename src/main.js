a40d974031f0ff4b51acbcd0bf05b0b06c91601f
Brittany Walker <breneewalker572@gmail.com>
2025-03-27 14:42:41 -0400

    Update shows

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot } from '@fortawesome/free-solid-svg-icons'
+import { faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faRepeat, faHeart,faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot)
+library.add(faRepeat, faHeart,faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
