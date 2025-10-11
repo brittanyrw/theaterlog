8b8c658184910189acc01c83b5c5f829234547bf
Brittany Walker <breneewalker572@gmail.com>
2025-10-10 22:44:54 -0400

    add shows

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross } from '@fortawesome/free-solid-svg-icons'
+import { faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross, faHeartCrack } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross)
+library.add(faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross, faHeartCrack)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
