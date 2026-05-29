7c069baf7b9c8a4cbb4467f568e97246f024b71b
Brittany Walker <breneewalker572@gmail.com>
2026-05-28 23:27:46 -0400

    update

--- src/main.js
+++ src/main.js
@@ -4,10 +4,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross, faHeartCrack, faPersonSkating } from '@fortawesome/free-solid-svg-icons'
+import { faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross, faHeartCrack, faPersonSkating, faPlane } from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross, faHeartCrack, faPersonSkating)
+library.add(faRepeat, faHeart, faCity, faCalendarCheck, faMoneyBillWave, faChair, faStar, faMusic, faHouseChimneyWindow, faBroom, faSeedling, faPenFancy, faFire, faUmbrella, faMicrophoneLines, faBook, faBookBookmark, faUtensils, faCrown, faBreadSlice, faHouse, faNewspaper, faChess, faEnvelope, faGuitar, faBookBible, faMagnifyingGlass, faCakeCandles, faRobot, faMugSaucer, faBuilding, faCross, faHeartCrack, faPersonSkating, faPlane)
 
 createApp(App)
   .component('font-awesome-icon', FontAwesomeIcon)
